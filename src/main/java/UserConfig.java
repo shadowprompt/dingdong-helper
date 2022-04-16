60674097f1437b8c4a1569085de4821438412b19
hxiong <hxiong@ctrip.com>
2022-04-16 19:11:47 +0800

    [fix]调整参数及openid

--- src/main/java/UserConfig.java
+++ src/main/java/UserConfig.java
@@ -1,6 +1,7 @@
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * 用户信息
@@ -12,11 +13,14 @@ public class UserConfig {
     public static final String cityId = "0101";//默认上海
 
     //站点id
-    public static final String stationId = "";
+    public static final String stationId = "5c04bd75716de1403a8b6797";
 
     //收货地址id
-    public static final String addressId = "";
+    public static final String addressId = "625968be2bbe330001e4128d";
 
+    // 构造随机的openId
+    public static final String randId = UUID.randomUUID().toString().substring(0, 5);
+    public static final String fakeOpenId = "osP8I0cMnJvcFssalHGhymedEucc".substring(0, 23) + randId;
     /**
      * 确认收货地址id和站点id
      * 每天抢之前先允许一下此接口 确认登录信息是否有效 如果失效了重新抓一次包
@@ -43,12 +47,13 @@ public class UserConfig {
         headers.put("referer", "https://servicewechat.com/wx1e113254eda17715/425/page-frame.html");
 
         // ------------  填入以下6项 上面不要动 ------------
-        headers.put("ddmc-device-id", "");
-        headers.put("cookie", "");
-        headers.put("ddmc-longitude", "");
-        headers.put("ddmc-latitude", "");
-        headers.put("ddmc-uid", "");
+        headers.put("ddmc-device-id", fakeOpenId);
+        headers.put("cookie", "DDXQSESSID=10d966e31314abe7525a46c5dc413df9"); //--
+        headers.put("ddmc-longitude", "121.401653");
+        headers.put("ddmc-latitude", "31.24744");
+        headers.put("ddmc-uid", "625968755c3f5d0001c33b85"); //
         headers.put("user-agent", "");
+//        headers.put("user-agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat");
         return headers;
     }
 
@@ -74,9 +79,9 @@ public class UserConfig {
         body.put("time", headers.get("ddmc-time"));
 
         // ------------  填入这3项上面不要动 ------------
-        body.put("s_id", "");
-        body.put("openid", "");
-        body.put("device_token", "");
+        body.put("s_id", "10d966e31314abe7525a46c5dc413df9"); // ---
+        body.put("openid", fakeOpenId);
+        body.put("device_token", "WHJMrwNw1k/FKPjcOOgRd+IO+egB0+hZPaHqL9kG5GY91qeyxLgYvk4Xgf7EkU/LRdLOjc0SRVt7fMtHNws1EEQU9Dzwb5+YqdCW1tldyDzmauSxIJm5Txg==1487582755342");
         return body;
     }
 }
